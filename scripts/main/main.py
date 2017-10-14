c99371a7705981d983c298fed82cd8b7f0087a21
Jonas Hielscher <benedikt.hielscher@st.ovgu.de>
2017-10-14 13:40:31 +0200

    Added new ImageProcessor UnitTests.

--- scripts/main/main.py
+++ scripts/main/main.py
@@ -1,2 +1,9 @@
+import os
+
+from scripts.image_processing.image_processing import ImageProcessor
+
 if __name__ == "__main__":
-    pass
+    image_path = os.path.join("../../data_storage/images/example_image_davinci.png")
+    image_processor = ImageProcessor(image_path)
+    print(image_processor.width)
+    print(image_processor.height)
--- tests/image_processing_test.py
+++ tests/image_processing_test.py
@@ -6,9 +6,12 @@ from scripts.image_processing.image_processing import ImageProcessor
 
 
 class ImageProcessorTest(unittest.TestCase):
+    """Tests the ImageProcessor with a real example image."""
     example_image_path = os.path.join("data_storage/images/example_image_davinci.png")
+    EXAMPLE_IMAGE_WIDTH = 800
+    EXAMPLE_IMAGE_HEIGHT = 800
 
-    def test_picture_load(self):
+    def test_picture_load_with_correct_path_should_not_throw_error(self):
         try:
             # ARRANGE
             image_processor = ImageProcessor()
@@ -20,6 +23,31 @@ class ImageProcessorTest(unittest.TestCase):
 
             # ASSERT
             self.assertTrue(False, traceback.format_exc())
+
+    def test_picture_load_with_wrong_path_should_throw_error(self):
+        try:
+            # ARRANGE
+            image_processor = ImageProcessor()
+
+            # ACT
+            image_processor.load_image(" ")
+
+        except FileNotFoundError:
+            self.assertTrue(True)
+
+    def test_picture_width(self):
+        # ARRANGE & ACT
+        image_processor = ImageProcessor(self.example_image_path)
+
+        # ASSERT
+        self.assertEqual(image_processor.width, self.EXAMPLE_IMAGE_WIDTH)
+
+    def test_picture_height(self):
+        # ARRANGE & ACT
+        image_processor = ImageProcessor(self.example_image_path)
+
+        # ASSERT
+        self.assertEqual(image_processor.height, self.EXAMPLE_IMAGE_HEIGHT)
 
 
 if __name__ == "__main__":
